66a4079065cb12d761ffeea4c0cc443f22752060
Yan Grenier <ygrenier@ygrenier.com>
2016-03-19 05:01:01 +0100

    Modification des styles. Amélioration de l'horloge

--- sources/config.php
+++ sources/config.php
@@ -17,7 +17,6 @@ if($command==="refresh-dashboard"){
     	<title>MakerBoard</title>
     	<!-- Bootstrap -->
      	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <link href="assets/css/main.css" rel="stylesheet" type="text/css"/>
 	<script src="assets/js/jquery-1.12.2.min.js"></script>
         <script src="assets/js/bootstrap.min.js"></script>
 </head>
